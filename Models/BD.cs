4500c304f5d5ceaa7ace8e03d76226ea5e3e07ee
ULISES ANIBAL BAAMONDE <45687573@ort>
2022-10-06 15:25:16 -0300

    Empezando la base de datos Commit

--- Models/BD.cs
+++ Models/BD.cs
@@ -8,5 +8,15 @@ namespace TPN07.Models;
 
 public class BD{
 
+private static string _connectionString = @"Server=A-PHZ2-CIDI-013;DataBase=AlmacenVideoJuegos;Trusted_Connection=True";
+
+public static List<Videojuego> ObtenerPreguntas(string nombre){
+    List<Videojuego> lista = new List<Videojuego>();
+    using(SqlConnection db = new SqlConnection(_connectionString)){   
+        string sql = "SELECT * FROM Videojuego WHERE Nombre = %@vNombre%";
+        lista = db.Query<Videojuego>(sql, new{vNombre = nombre}).ToList();
+    }
+    return lista;
+}
 
 }
--- Models/Videojuego.cs
+++ Models/Videojuego.cs
@@ -4,6 +4,97 @@ namespace TP_Final.Models;
 
 public class Videojuego
 {
+    private int _idVideojuego;
+    private int _idEmpresa;
+    private DateTime _fechaLanzamiento;
+    private int _idCategoria;
+    private string _nombre;
+    private string _descripcion;
+    private string _logo;
+    private string _caratula;
+    private string _banner;
 
+    public int IdVideojuego
+    {
+        get{
+            return _idVideojuego;
+        }
+        set{
+            _idVideojuego = value;
+        }
+    }
+    public int IdEmpresa
+    {
+        get{
+            return _idEmpresa;
+        }
+        set{
+            _idEmpresa = value;
+        }
+    }
+    public DateTime FechaLanzamiento
+    {
+        get{
+            return _fechaLanzamiento;
+        }
+        set{
+            _fechaLanzamiento = value;
+        }
+    }
+
+    public int IdCategoria
+    {
+        get{
+            return _idCategoria;
+        }
+        set{
+            _idCategoria = value;
+        }
+    }
+    public string Nombre
+    {
+        get{
+            return _nombre;
+        }
+        set{
+            _nombre = value;
+        }
+    }
+    public string Descripcion
+    {
+        get{
+            return _descripcion;
+        }
+        set{
+            _descripcion = value;
+        }
+    }
+    public string Logo
+    {
+        get{
+            return _logo;
+        }
+        set{
+            _logo = value;
+        }
+    }
+     public string Caratula
+    {
+        get{
+            return _caratula;
+        }
+        set{
+            _caratula = value;
+        }
+    }
+     public string Banner
+    {
+        get{
+            return _banner;
+        }
+        set{
+            _banner = value;
+        }
+    }
 
 }
